d9a9a68c64eb10225e74cbe10541758a5f77d6e9
Dorian Mutti <dorianmutti@gmail.com>
2024-07-01 10:21:21 -0400

    sfpinfpi

--- src/Units/lengthConversion.java
+++ src/Units/lengthConversion.java
@@ -16,7 +16,22 @@ public class lengthConversion {
         
         String selection = "";
         
-        System.out.println("Enter the unit you wish to convert from");
-        selection = Scanner.nextLine();
+        System.out.println("Select your starting unit: ");
+        System.out.println("1. Millimeters");
+        System.out.println("2. Centimeters");
+        System.out.println("3. Metres");
+        System.out.println("4. Kilometers");
+        System.out.println("5. Inches");
+        System.out.println("6. Feet");
+        System.out.println("7. Yards");
+        System.out.println("8. Miles");
+        System.out.println("9. Nautical Miles");
+        System.out.println("10. Quit");
+        selection = scanner.nextLine();
+
+        if (selection.equalsIgnoreCase("Millimeters") || selection.equalsIgnoreCase("millimeters")
+        || selection.equalsIgnoreCase("mm") || selection.equalsIgnoreCase("MM")) {
+            
+        }
     }
 }
